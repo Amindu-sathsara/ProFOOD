74b61e3ae105f4def79318b273ae2b1c9b57036f
Amindu Sathsara <dapsathsara@std.appsc.sab.ac.lk>
2024-09-22 09:29:30 +0530

    created basic react element using just JSX syntax and also created react functional components

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
+//
+
+
+
 import React from 'react';  // Correct import for React
 import ReactDOM from 'react-dom/client';  // Correct import for ReactDOM
 
 
 //React.createElement=>ReactElement(Js-Object)=> then -> ReactElement(render)
 const heading = React.createElement("h1",{id:"heading"},"Namasthe react 💡💡");
-const rootElement = document.getElementById("root");
-const root =ReactDOM.createRootElement(heading);
-root.render(rootElement);
+
+
+const root =ReactDOM.createRoot( document.getElementById("root2"));
+root.render(heading);
 
 
 //Jsx transfiled before it reached the js =>Parcel-->Babel
 //Jsx =>React transfiles it to React.createElement =>ReactElewment==Wich is js Object and =>HTML element (render)
 
 //JSX is HTML-like or XML-like element not the exact one 
-const jsxHeading =<h1>Here I am creatd react heading element using JSX syntax </h1>
+/*const jsxHeading =<h1>Here I am creatd react heading element using JSX syntax </h1>
 console.log(jsxHeading); 
 
 const root2=ReactDOM.createRoot(document.getElementById("root2"));
-root2.render(root2);
+root2.render(root2);*/
+
+const Heading2=()=>{
+    <h1>Here , I am creating react Element</h1>
+}
+
+
+
+
 
