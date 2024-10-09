7ea08f4011fa4e0f6cbf85e4f7d7d2897ce72184
Amindu Sathsara <dapsathsara@std.appsc.sab.ac.lk>
2024-10-09 08:28:12 +0530

    fetching the data via API and render components with those data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 import resList from "../utils/mockData";
 
@@ -8,7 +8,7 @@ import resList from "../utils/mockData";
 const Body=()=>{
 
     //Local state variable -super powerful variables 
-    const [resList1,setResList1]=useState(resList);
+    const [resList1,setResList1]=useState([]);
     
     /*
     to understand what is happening in the useState -use below wierd syntax 
@@ -19,6 +19,21 @@ const Body=()=>{
                   const restList2=arr[0];
                   const setResList=arr[1];
                   */
+    useEffect(()=>{
+        fetchData();
+        console.log("useEffect is called");
+    },[])
+
+
+    const fetchData=async ()=>{
+        const response=await fetch("https://dummyjson.com/carts");
+        const jsonData=await response.json();
+        console.log(jsonData);
+        //console.log("Remove the link that used to fetch data")
+        setResList1(jsonData?.carts[19]?.products);
+        
+          
+    }
     return (
         <div className="body">
             <div className="search-container">
@@ -27,7 +42,7 @@ const Body=()=>{
             <div className="filter-container">
                 <button className="filter-btn" onClick={()=>{
 
-                    const filteredList=resList1.filter((res)=>res.rating>=4
+                    const filteredList=resList1.filter((res)=>res.quantity>=4
                 );
                 
                 setResList1(filteredList);
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,10 +4,10 @@ const RestaurantCard=(props)=>{   // form the campornets props in JSX come over 
   
       return (
           <div className="restaurant-card" >
-              <img className="res-card-img" alt="restaurant card image is over there" src={resData.image}/>
-              <h2>{resData.name}</h2>
-              <p> {resData.rating}</p>
-              <p>{resData.description}</p>
+              <img className="res-card-img" alt="restaurant card image is over there" src={resData.thumbnail}/>
+              <h2>{resData.title}</h2>
+              <p> Rs.{resData.price} only</p>
+              <p>{resData.quantity} items available</p>
               <button>Order Now</button>
           </div>
       )
