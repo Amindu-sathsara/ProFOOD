c52238f8eea303ec4801ffa4db871abfded834e6
Amindu Sathsara <dapsathsara@std.appsc.sab.ac.lk>
2024-12-22 22:49:27 +0530

    Updating the cartSlice

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -8,14 +8,25 @@ const cartSlice = createSlice({
     reducers:{
         addItem:(state,action)=>{
             //Noted: Mutating the state here    >>> I mean we are directly modifying the state here 
-
+            //Immer.js library take care of the mutation and all the action and behind the scene redux toolkit use immer.js library
+            
             state.items.push(action.payload);
+
+
+            //Vanial redux or older way of redux => DON'T MUTATE THE STATE directly , returing is also mandatory
+            //const newState=[...state]
+            //newState.items.push(action.payload);
+            //return newState;
+
         },
         removeItem:(state,action)=>{
             state.items.pop();
         },
         clearCart:(state)=>{
             state.items.length=0;
+
+            // there is a alternative for the same thing for above line 26
+            //return {items:[]}      //this new [] will be replaced inside original state =[] 
         }
     },
     });
